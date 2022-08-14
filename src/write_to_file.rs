d3eeec1eb9b6620be3cb29e0cca0f6c9c9ea0ea3
jmaczan <jedrzejpawel@maczan.pl>
2022-08-14 19:36:10 +0200

    Extract functions in write_to_file

--- src/write_to_file.rs
+++ src/write_to_file.rs
@@ -4,14 +4,20 @@ use std::{
 };
 
 pub fn write_to_file(path_to_directory: &str, file_name: &str, file_content: String) {
-    // Create a temporary file.
-    // Open a file in write-only (ignoring errors).
-    // This creates the file if it does not exist (and empty the file if it exists).
+    create_directory_if_not_exist(path_to_directory);
+    write_content_to_file(path_to_directory, file_name, file_content);
+}
+
+fn create_directory_if_not_exist(path_to_directory: &str) {
     DirBuilder::new()
         .recursive(true)
         .create(path_to_directory)
         .unwrap();
-    let mut file = File::create(path_to_directory.to_owned() + "/" + file_name).unwrap();
-    file.write_all(file_content.as_bytes())
+}
+
+fn write_content_to_file(path_to_directory: &str, file_name: &str, file_content: String) {
+    File::create(path_to_directory.to_owned() + "/" + file_name)
+        .unwrap()
+        .write_all(file_content.as_bytes())
         .expect("Failed to write to a file");
 }
