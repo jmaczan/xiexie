835657b9f371ac797d05d22b3368512d4e08518e
jmaczan <jedrzejpawel@maczan.pl>
2022-08-15 17:01:43 +0200

    Clear dist folder; WIP Append link to css files inside built html files

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,17 @@ fn main() {
     println!("Starting xiexie 谢谢!");
 
     let source_directory = "./src/".to_owned() + "sample-source-folder";
+    let target_directory = "./dist";
     let skeleton_file_name = "skeleton.html";
     let source_directory_name_length = source_directory.len() as usize;
     let skeleton_file_path = source_directory.to_owned() + "/" + skeleton_file_name;
+    let html_extension = ".html";
 
     println!("Reading a website skeleton... 💀🦴");
     let skeleton_html_content = read_from_file::read_from_file(skeleton_file_path.as_str());
-    // println!("With text:\n{skeleton_html_content}");
     println!("Done 💮");
 
-    let raw_pages_list = match get_pages_list::get_pages_list(source_directory) {
+    let raw_files_list = match get_pages_list::get_pages_list(source_directory) {
         Ok(pages_list) => pages_list,
         Err(_) => {
             println!("I couldn't find source files to generate the website.");
@@ -23,30 +24,34 @@ fn main() {
         }
     };
 
-    let pages_list = raw_pages_list
+    let files_list = raw_files_list
         .into_iter()
-        .map(|file| String::from(file.to_str().unwrap()))
+        .map(|file| String::from(file.to_str().unwrap()));
+    println!("{:?}", files_list);
+    let pages_list = files_list
         .filter(|file| {
-            file.to_lowercase().ends_with(".html") && !file.ends_with(skeleton_file_name)
+            file.to_lowercase().ends_with(html_extension) && !file.ends_with(skeleton_file_name)
         })
         .collect::<Vec<String>>();
 
-    println!("{:?}", pages_list);
-
     pages_list.into_iter().for_each(|subpage_path| {
         let mut subpage_content = read_from_file::read_from_file(subpage_path.as_str());
-
+        let subpage_file_name = subpage_path.get(source_directory_name_length..).unwrap();
+        let subpage_name = subpage_file_name
+            .get(1..subpage_file_name.len() - html_extension.len())
+            .unwrap();
         subpage_content = skeleton_html_content
             .as_str()
             .replace("xiexie::title", "New title")
-            .replace("xiexie::body", subpage_content.as_str());
+            .replace("xiexie::body", subpage_content.as_str())
+            .replace(
+                "xiexie::css",
+                ("<link rel=\"stylesheet\" href=\"".to_owned() + subpage_name + ".css\" />")
+                    .as_str(),
+            );
 
-        write_to_file::write_to_file(
-            "./dist",
-            subpage_path.get(source_directory_name_length..).unwrap(),
-            subpage_content,
-        );
+        write_to_file::write_to_file(target_directory, subpage_file_name, subpage_content);
     });
 
-    println!("Your website is ready to use! All generated files are inside the dist directory. xiexie! 谢谢!");
+    println!("Your website is ready to use! All generated files are inside the {} directory. xiexie! 谢谢!", target_directory);
 }
--- src/write_to_file.rs
+++ src/write_to_file.rs
@@ -1,22 +1,32 @@
+use std::path::Path;
 use std::{
-    fs::{DirBuilder, File},
+    fs::{remove_dir_all, DirBuilder, File},
     io::Write,
 };
 
-pub fn write_to_file(path_to_directory: &str, file_name: &str, file_content: String) {
-    create_directory_if_not_exist(path_to_directory);
-    write_content_to_file(path_to_directory, file_name, file_content);
+pub fn write_to_file(directory_path: &str, file_name: &str, file_content: String) {
+    remove_directory_with_content(directory_path);
+    create_directory(directory_path);
+    write_content_to_file(directory_path, file_name, file_content);
 }
 
-fn create_directory_if_not_exist(path_to_directory: &str) {
+fn remove_directory_with_content(directory_path: &str) {
+    if !Path::new(directory_path).exists() {
+        return;
+    }
+
+    remove_dir_all(directory_path).expect("Couldn't remove dist.");
+}
+
+fn create_directory(directory_path: &str) {
     DirBuilder::new()
         .recursive(true)
-        .create(path_to_directory)
+        .create(directory_path)
         .unwrap();
 }
 
-fn write_content_to_file(path_to_directory: &str, file_name: &str, file_content: String) {
-    File::create(path_to_directory.to_owned() + "/" + file_name)
+fn write_content_to_file(directory_path: &str, file_name: &str, file_content: String) {
+    File::create(directory_path.to_owned() + "/" + file_name)
         .unwrap()
         .write_all(file_content.as_bytes())
         .expect("Failed to write to a file");
