e4c7313accb1ee46746fbdaa971222e4088a3ecf
jmaczan <jedrzejpawel@maczan.pl>
2022-08-20 23:28:47 +0200

    Split generate_assets to functions

--- src/generate_assets.rs
+++ src/generate_assets.rs
@@ -1,23 +1,26 @@
-use crate::{Args, ALLOWED_ASSETS_EXTENSIONS, HTML_EXTENSION};
+use crate::{Args, ALLOWED_ASSETS_EXTENSIONS};
 use clap::Parser;
 use std::fs;
 
 pub fn generate_assets(files_list: Vec<String>) {
     files_list
         .into_iter()
-        .filter(|file| {
-            !file.to_lowercase().ends_with(HTML_EXTENSION)
-                && ALLOWED_ASSETS_EXTENSIONS
-                    .into_iter()
-                    .any(|extension| file.to_lowercase().ends_with(extension))
-        })
+        .filter(|file| only_allowed_assets(file))
         .for_each(|file| {
-            fs::copy(
-                &file,
-                Args::parse().target.to_owned()
-                    + "/"
-                    + file.get(Args::parse().source.len()..).unwrap(),
-            )
-            .unwrap();
+            copy_to_target_directory(file);
         });
+}
+
+fn copy_to_target_directory(file: String) {
+    fs::copy(
+        &file,
+        Args::parse().target.to_owned() + "/" + file.get(Args::parse().source.len()..).unwrap(),
+    )
+    .unwrap();
+}
+
+fn only_allowed_assets(file: &String) -> bool {
+    ALLOWED_ASSETS_EXTENSIONS
+        .into_iter()
+        .any(|extension| file.to_lowercase().ends_with(extension))
 }
