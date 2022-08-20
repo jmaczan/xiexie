c103886adf14b5dcc8089ead566f44d79c98f85d
jmaczan <jedrzejpawel@maczan.pl>
2022-08-20 23:39:16 +0200

    Extract functions from generate_htmls

--- src/generate_htmls.rs
+++ src/generate_htmls.rs
@@ -24,30 +24,45 @@ fn generate_html_file(file_path: String) {
         .get(..subpage_file_name.len() - HTML_EXTENSION.len())
         .unwrap();
 
-    let raw_json_file_content = io::read_from_file(
-        (source_directory_path.to_owned() + subpage_name + JSON_EXTENSION).as_str(),
-    );
-    let json_file_content = serde_json::from_str::<JSON>(&raw_json_file_content).unwrap();
+    let configuration = read_configuration(source_directory_path, subpage_name);
 
-    if json_file_content.purpose == TEMPLATE_PURPOSE.to_owned() {
+    if configuration.purpose == TEMPLATE_PURPOSE.to_owned() {
         return;
     }
 
-    let template_file_name = json_file_content.template.as_str();
-    let template_file_path = source_directory + "/" + template_file_name;
-
-    let template_html_content = io::read_from_file(template_file_path.as_str());
+    let template_html_content = read_template(&configuration, source_directory);
 
     let css_file_path = source_directory_path.to_owned() + subpage_name + CSS_EXTENSION;
     let has_css_file = Path::new(&css_file_path).exists();
-    let css_file_html_link = "<link rel=\"stylesheet\" href=\"".to_owned()
-        + subpage_name.get(1..).unwrap()
-        + CSS_EXTENSION
-        + "\" />";
+    let css_file_html_link = build_css_link(subpage_name);
 
     let mut subpage_content = io::read_from_file(file_path.as_str());
 
-    subpage_content = template_html_content
+    append_css_link(
+        &mut subpage_content,
+        template_html_content,
+        has_css_file,
+        css_file_html_link,
+    );
+
+    for (key, value) in configuration.fields.iter().flat_map(|field| field.iter()) {
+        subpage_content = subpage_content.replace((TAG_PREFIX.to_owned() + key).as_str(), value);
+    }
+
+    io::write_to_file(
+        Args::parse().target.as_str(),
+        subpage_file_name,
+        subpage_content,
+    );
+}
+
+fn append_css_link(
+    subpage_content: &mut String,
+    template_html_content: String,
+    has_css_file: bool,
+    css_file_html_link: String,
+) {
+    *subpage_content = template_html_content
         .as_str()
         .replace(BODY_TAG, subpage_content.as_str())
         .replace(
@@ -57,18 +72,26 @@ fn generate_html_file(file_path: String) {
                 false => "",
             },
         );
+}
 
-    for (key, value) in json_file_content
-        .fields
-        .iter()
-        .flat_map(|field| field.iter())
-    {
-        subpage_content = subpage_content.replace((TAG_PREFIX.to_owned() + key).as_str(), value);
-    }
+fn build_css_link(subpage_name: &str) -> String {
+    "<link rel=\"stylesheet\" href=\"".to_owned()
+        + subpage_name.get(1..).unwrap()
+        + CSS_EXTENSION
+        + "\" />"
+}
 
-    io::write_to_file(
-        Args::parse().target.as_str(),
-        subpage_file_name,
-        subpage_content,
+fn read_template(configuration: &JSON, source_directory: String) -> String {
+    let template_file_name = configuration.template.as_str();
+    let template_file_path = source_directory + "/" + template_file_name;
+    let template_html_content = io::read_from_file(template_file_path.as_str());
+    template_html_content
+}
+
+fn read_configuration(source_directory_path: &str, subpage_name: &str) -> JSON {
+    let raw_json_file_content = io::read_from_file(
+        (source_directory_path.to_owned() + subpage_name + JSON_EXTENSION).as_str(),
     );
+
+    serde_json::from_str::<JSON>(&raw_json_file_content).unwrap()
 }
